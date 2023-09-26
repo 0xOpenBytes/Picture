7c5d247b92260db8ee3f4be52a2d6dcdbb2c2d97
Leif <zmeriksen@icloud.com>
2023-09-26 16:37:28 -0600

    init

---
+++ Package.swift
@@ -0,0 +1,31 @@
+// swift-tools-version: 5.9
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Picture",
+    platforms: [
+        .iOS(.v16),
+        .watchOS(.v9),
+        .macOS(.v13),
+        .tvOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "Picture",
+            targets: ["Picture"]
+        )
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "Picture"),
+        .testTarget(
+            name: "PictureTests",
+            dependencies: ["Picture"]
+        )
+    ]
+)
---
+++ Sources/Picture/Picture.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+public struct Picture: View {
+    public var body: some View {
+        Image(systemName: "photo.artframe")
+    }
+}
+
+#Preview {
+    Picture()
+}
