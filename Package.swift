13dadb8200e0157258ce563c5bd5776ffc7bd128
Leif <zmeriksen@icloud.com>
2023-10-07 12:02:38 -0600

    Only support iOS 17 and Swift 5.9

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -6,10 +6,7 @@ import PackageDescription
 let package = Package(
     name: "Picture",
     platforms: [
-        .iOS(.v16),
-        .watchOS(.v9),
-        .macOS(.v13),
-        .tvOS(.v16)
+        .iOS(.v17)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
