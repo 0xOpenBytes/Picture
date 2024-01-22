fce9f67d65e91d594ef5deda7dbdbbdfcb9b732c
bellatrick <busayosamuel2016@gmail.com>
2024-01-22 11:04:49 +0100

    Added documentation for picturesource and picturesourceimage files

--- Sources/Picture/Public/PictureSourceImage.swift
+++ Sources/Picture/Public/PictureSourceImage.swift
@@ -1,7 +1,20 @@
+// PictureSourceImage.swift
+
 import SwiftUI
+
+/**
+ `PictureSourceImage` is a view representing an image loaded from a `PictureSource`.
+ This view provides a way to display images interactively, adjusting to fit the available space.
+
+*/
 
 public struct PictureSourceImage: View {
     @ObservedObject private var viewModel: InteractableImageViewModel
+    /**
+    Initializes a `PictureSourceImage` view with a specified picture source.
+
+    - Parameter source: The source of the picture (local or remote).
+    */
 
     public init(
         source: PictureSource
@@ -10,6 +23,7 @@ public struct PictureSourceImage: View {
             initialValue: InteractableImageViewModel(source: source)
         )
     }
+    /// The body of the `PictureSourceImage` view.
 
     public var body: some View {
         if let image = viewModel.image {
